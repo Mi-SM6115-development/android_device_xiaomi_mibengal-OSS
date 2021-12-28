f6ba25eb996ae1990d8b27490169511ad3240384
RE800T <brutalman122@gmail.com>
2021-12-28 06:41:20 +0000

    mibengal: device: Build android.hardware.nfc@1.0 * Switch to android.hardware.nfc@1.0
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- device.mk
+++ device.mk
@@ -419,7 +419,8 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
+    android.hardware.nfc@1.0 \
+    android.hardware.nfc@1.0-service \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
